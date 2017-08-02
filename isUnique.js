bab38fafc44060cea10f9018b6db2486d09708dc
tommytnez <tommytnez@yahoo.com>
2017-08-02 16:15:13 -0700

    1.1 isUnique ctci

---
+++ isUnique.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function isUnique(string){
+  var chars = {};
+  for (var i = 0; i < string.length; i++){
+    if(chars[string[i]]){
+      return false;
+    }
+    chars[string[i]] = true;
+  }
+  return true;
+}
+
+isUnique('abcdefgh');///true
+isUnique('ijkklmnopqrs');//false
